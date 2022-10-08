1d09cb2c94413f68fc206d55aa02231d3a7f6d8e
fancyday <2414786389@qq.com>
2022-10-08 16:55:33 +0800

    first

--- src/main/java/fit/fancyday/controller/Login.java
+++ src/main/java/fit/fancyday/controller/Login.java
@@ -1,2 +1,14 @@
-package fit.fancyday.controller;public class Login {
+package fit.fancyday.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class Login {
+    @GetMapping("/auth")
+    public String login(){
+        return "登录成功~~";
+    }
 }
